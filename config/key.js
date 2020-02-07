f3d11764d252b4609e89724787eac3d0fc99d2e0
SlimeVidas <dahan.isaac@gmail.com>
2020-02-07 16:19:27 +0200

    config-mLab-and-create-config-file

---
+++ config/key.js
@@ -0,0 +1,8 @@
+const dbuser = 'isaac';
+const dbpassword = 'trottinette';
+
+
+module.exports = {
+    mongoURI: "mongodb+srv://${dbuser}:${dbpassword}@cluster0-3or9x.mongodb.net/test?retryWrites=true&w=majority" 
+  };
+
