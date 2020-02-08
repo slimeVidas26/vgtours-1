576d7da8770e60e1101ac610d2f3b59849f5d140
SlimeVidas <dahan.isaac@gmail.com>
2020-02-08 22:14:12 +0200

    Setting up our API routes

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,9 @@
-// const dbuser = 'isaac';
-// const dbpassword = 'trottinette';
+ const dbuser = 'isaac';
+ const dbpassword = 'trottinette';
 //   const  mongoURI = `mongodb+srv://${dbuser}:${dbpassword}@cluster0-3or9x.mongodb.net/test?retryWrites=true&w=majority` ;
 //   module.export = mongoURI;
 
   module.exports = {
-    mongoURI: "mongodb+srv://isaac:trottinette@cluster0-3or9x.mongodb.net/test?retryWrites=true&w=majority" 
+    mongoURI: `mongodb+srv://${dbuser}:${dbpassword}@cluster0-3or9x.mongodb.net/test?retryWrites=true&w=majority`
   };
 
