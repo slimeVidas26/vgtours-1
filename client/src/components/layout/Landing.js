d37c5dd19219d2625021553ed13a92f8ceddad75
SlimeVidas <dahan.isaac@gmail.com>
2020-02-16 18:55:42 +0200

    final

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -37,7 +37,7 @@ class Landing extends Component {
                   borderRadius: "3px",
                   letterSpacing: "1.5px"
                 }}
-                className="btn btn-large btn-flat waves-effect white black-text"
+                className="btn btn-large waves-effect waves-light hoverable green accent-3"
               >
                 Log In
               </Link>
