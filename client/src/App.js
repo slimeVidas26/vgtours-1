94c2b7b3acad8e6e1a1263d69677dc20aa918883
SlimeVidas <dahan.isaac@gmail.com>
2020-03-01 18:14:36 +0200

    test-semantic-ok

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
 import './App.css';
-import 'semantic-ui-css/semantic-min-css'
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
@@ -14,6 +13,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
+import ModalExample from "./components/semantic-ui-tests/ModalExample"
 
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
@@ -43,6 +43,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar />
+          <ModalExample/>
           <Route exact path="/" component={Landing} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
