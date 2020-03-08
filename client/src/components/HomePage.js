e867685449bed83e9739d898148b55958f75dfd4
SlimeVidas <dahan.isaac@gmail.com>
2020-03-08 18:49:03 +0200

    test

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import '../assets/less/base.min.css'
 import '../assets/less/header.min.css'
 import '../assets/less/theme.min.css'
+import '../assets/icon/style.css'
+
 import '../assets/images/ico/favicon.ico'
 
 
