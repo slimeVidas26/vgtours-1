8fe0ec6ad07888721bb1fe4593b0763f6fcd3920
SlimeVidas <dahan.isaac@gmail.com>
2020-04-21 22:05:08 +0300

    cookie session

--- auth-net-ninja/n-app.js
+++ auth-net-ninja/n-app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes')
 const app = express();
+const passport = require('passport');
 const passportSetup  = require('./config/passport-setup')
 const mongoose = require('mongoose');
-const keys = require('./config/keys')
+const keys = require('./config/keys');
+const cookieSession  = require('cookie-session');
 
 // set template engine
-app.set('view engine' , "ejs")
+app.set('view engine' , "ejs");
+
+//cookie-session
+app.use(cookieSession({
+  maxAge : 24*60*60*1000,
+  keys : [keys.session.cookieKey]
+}))
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
