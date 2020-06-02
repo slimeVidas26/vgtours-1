f2df6cfd13cf0ee6ba53a2175f1821fe71f6aa36
SlimeVidas <dahan.isaac@gmail.com>
2020-06-02 22:17:54 +0300

    test-semantic

---
+++ client/src/components/elements/Btn.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+const ButtonExampleButton = () =>
+ <div>
+    <Button content='Primary' primary />
+    <Button content='Secondary' secondary />
+  </div>
+ {/* <Button>Click Here</Button> */}
+export default ButtonExampleButton
