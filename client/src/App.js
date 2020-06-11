64d610874957fe4772d34b375a80da8bb238ac34
SlimeVidas <dahan.isaac@gmail.com>
2020-06-11 21:58:19 +0300

    load-script-is=ok

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,13 @@ import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
-// import { $ } from "jquery";
+import {loadScriptHead , loadScript , scriptsArray} from "./scriptsJS"
  import './App.css';
 
   import './assets/less/base.css'
   import './assets/less/header.css'
   import './assets/less/theme.css'
   import './assets/icon/style.css'
-
- 
-
-
-
 
 
 // import Navbar from "./components/layout/Navbar";
@@ -33,9 +28,6 @@ import SignUpPage from './components/auth/SignUpPage';
 // import InstagramAuth from './components/auth/InstagramAuth'
 // import GithubAuth from './components/auth/GithubAuth'
 import Profile from './components/auth/Profile'
-
-
-
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
 import HomePage from "./components/HomePage";
@@ -64,56 +56,23 @@ if (localStorage.jwtToken) {
   }
 }
 
-
-
 class App extends Component {
 
-   loadScriptHead = function(src) {
-    const tag = document.createElement('script');
-    tag.async = false;
-    tag.src = src;
-    document.head.appendChild(tag);
-  }
-
-  loadScript = function(src) {
-    const tag = document.createElement('script');
-    tag.async = false;
-    tag.src = src;
-    document.body.appendChild(tag);
-  }
-   
- 
-
   componentDidMount(){
-    this.loadScriptHead('/assets/library/modernizr-custom.js')
-    this.loadScript('/assets/library/jquery-2.2.0.min.js')
-    this.loadScript('/assets/library/flexmenu.js')
-    this.loadScript('/assets/library/nouislider.min.js')
-    this.loadScript('/assets/library/wNumb.js')
-    this.loadScript('/assets/library/jrespond.min.js')
-    this.loadScript('/assets/library/scrollspy.min.js')
-    this.loadScript('/assets/library/visibility.js')
-    this.loadScript('/assets/library/accordion.js')
-    this.loadScript('/assets/library/dropdown-custom.js')
-    this.loadScript('/assets/library/sticky.js')
-    this.loadScript('/assets/library/page-transition.js')
-    this.loadScript('/assets/library/checkbox.js')
-    this.loadScript('/assets/library/transition.js')
-    this.loadScript('/assets/library/sidebar.js')
-    this.loadScript('/assets/library/modal.js')
-    this.loadScript('/assets/library/dimmer.js')
 
-      //Datepicker
-    this.loadScript('/assets/library/popup.js')
-    this.loadScript('/assets/library/calendar.js')
-    this.loadScript('/assets/library/slick.js')
-    this.loadScript('/assets/library/jquery.fancybox.js')
-    this.loadScript('/assets/library/highlight.pack.js')
-    this.loadScript('/assets/library/clipboard.min.js')
-    this.loadScript('/assets/library/header.js')
-    this.loadScript('/assets/library/functions.js') 
+     const res = scriptsArray.map((item , index)=>{
+       if(index === 0){
+        return loadScriptHead(`/assets/library/${item}`)
+       }
+       else { 
+        return loadScript(`/assets/library/${item}`)
+   
+       }
+     
+    })
+
+    
   }
-
 
   render() {
     return (
