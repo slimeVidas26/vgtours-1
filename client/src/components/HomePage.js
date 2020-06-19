94e54a9cfd65841feee7bb3bed0a02ae4159c9f0
SlimeVidas <dahan.isaac@gmail.com>
2020-06-19 15:07:57 +0300

    test

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -26,7 +26,7 @@ class HomePage extends Component {
         return (
     <Fragment>
     <JsContextProvider>
-    <Header/>
+      <Header/>
       <HeroSearchFullPage/>
       <UiLayoutTop/>
       <Wishlist/>           
