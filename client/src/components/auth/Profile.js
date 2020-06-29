85147f1756a9dd9bb9f80d7f521d21f6655351c3
SlimeVidas <dahan.isaac@gmail.com>
2020-06-29 22:09:50 +0300

    create userInfo component

--- client/src/components/auth/Profile.js
+++ client/src/components/auth/Profile.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { socialLoginUser , socialLogoutUser } from "../../actions/authActions";
 import { withRouter, Switch} from "react-router-dom";
-import Loader from 'react-loader-spinner'
+import UserInfo from './UserInfo'
 
 const GOOGLE = "google-auth"
 const TWITTER = "twitter-auth"
@@ -15,32 +15,22 @@ const AMAZON = "amazon-auth"
 
 
 export class Profile extends Component {
+ 
     constructor() {
         super();
         this.state = {
          
         };
+        console.log(this.props)
         
       }
 
     
 
       formatUser(obj){
-         //const obj = {a:1 , b:2 , c:3}
-        // for (const [ key, value ] of Object.entries(x)) {
-        //   // do something with `key` and `value`
-        //   console.log(key , value)
-        //   console.log("length" , Object.length)
-        //   return `${key} ${value}`
-          // const k = Object.keys(obj);
-          // console.log("k" , k)
-          // const v = Object.values(obj);
-          // console.log("v" , v)
+        
            const userEntries = Object.entries(obj);
            console.log("userEntries" , userEntries)
-           
-          // console.log("e" , e)
-          // console.log(typeof(e))
           const result = userEntries
           .filter((item , i , arr)=>{
             return arr[i][0] !== "__v" && arr[i][0] !== "isAuthenticated"
@@ -67,15 +57,9 @@ export class Profile extends Component {
 
                             }
 
-          
-            
+        
       
-
-     
-
-      // 
-       getProfileNetworkUser() {
-
+       getProfileNetworkUser = () =>{
         const {handle} = this.props.match.params
         console.log("handle",{handle})
         const { user } = this.props.auth;
@@ -83,132 +67,17 @@ export class Profile extends Component {
         switch (handle) {
           
           case GOOGLE:
+            case AMAZON:
+              case TWITTER:
+                case FACEBOOK:
+                  case SPOTIFY:
+                    case INSTAGRAM:
+                      case GITHUB:
             return (
-              this.formatUser(user)  
-           );
-
-           case AMAZON:
-            return (
-              this.formatUser(user)  
-           );
-
-            case TWITTER:
-              return (
-                this.formatUser(user)  
-             );
-            // return (
+              this.formatUser(user) 
               
-            //  <div>
-            //       <h4>
-            //         <b>Hey there,</b> {user.name} <br/>
-            //         <b>Your ID is : ,</b> {user._id} <br/>
-            //         <b>Your TwitterId  is : ,</b> {user.twitterId} <br/>
-            //         <b>Your screenName  is : ,</b> {user.screenName} <br/>
-            //         <b>Date  is : ,</b> {user.date} <br/>
-            //         <img src={user.thumbnail} alt=""/>
 
-            //         <p className="flow-text grey-text text-darken-1">
-            //           You are logged into vg-tours{" "}
-            //           <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-            //         </p>
-            //       </h4>
-            //       </div>
-            // );
-
-            case FACEBOOK:
-              return (
-                this.formatUser(user)  
-             );
-            // return (
-            //  <div>
-            //       <h4>
-            //         <b>Your displayName : </b> {user.displayName} <br/>
-            //         <b>Your ID is :</b> {user._id} <br/>
-            //         <b>Your facebookId  is :</b> {user.facebookId} <br/>
-            //         <b>Your location  is :</b> {user.location} <br/>
-            //         <b>Date  is :</b> {user.date} <br/>
-            //         <b>provider  is : </b> {user.provider} <br/>
-            //         <img src={user.thumbnail} width = "100" alt="facebook thumbnail"/>
-
-
-            //         <p className="flow-text grey-text text-darken-1">
-            //           You are logged into vg-tours{" "}
-            //           <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-            //         </p>
-            //       </h4>
-            //       </div>
-            // );
-
-            case SPOTIFY:
-              return (
-                this.formatUser(user)  
-             );
-            // return (
-            //  <div>
-                    
-            //         <h4>
-            //           <b>Hey there,</b> {user.displayName} <br/>
-            //           <b>Your userName is : </b> {user.userName} <br/>
-            //           <b>Your ID is : </b> {user._id} <br/>
-            //           <b>Your spotifyId  is : </b> {user.spotifyId} <br/>
-            //           <b>Your country  is : </b> {user.country} <br/>
-            //           <b>Date  is : </b> {user.date} <br/>
-            //           <b>provider  is : </b> {user.provider} <br/>
-            //           <img src={user.thumbnail} width = "100" alt=""/>
-            //           <p className="flow-text grey-text text-darken-1">
-            //             You are logged into vg-tours{" "}
-            //             <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-            //           </p>
-            //         </h4>
-            //         </div>
-            // );
-            case INSTAGRAM:
-              return (
-                this.formatUser(user)  
-             );
-            // return (
-            // <div>
-                    
-            //         <h4>
-            //           <b>Hey there,</b> {user.displayName} <br/>
-            //           <b>Your userName is : </b> {user.userName} <br/>
-            //           <b>Your ID is : </b> {user._id} <br/>
-            //           <b>Your instagramId  is : </b> {user.instagramId} <br/>
-            //           <b>Your country  is : </b> {user.country} <br/>
-            //           <b>Date  is : </b> {user.date} <br/>
-            //           <b>provider  is : </b> {user.provider} <br/>
-            //           <img src={user.thumbnail} width = "100" alt=""/>
-            //           <p className="flow-text grey-text text-darken-1">
-            //             You are logged into vg-tours{" "}
-            //             <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-            //           </p>
-            //         </h4>
-            //         </div>
-            // );
-            case GITHUB:
-              return (
-                this.formatUser(user)  
-             );
-            // return (
-            //         <div>
-            //         <h4>
-            //           <b>Hey there,</b> {user.displayName} <br/>
-            //           <b>Your userName is : </b> {user.userName} <br/>
-            //           <b>Your ID is : </b> {user._id} <br/>
-            //           <b>Your githubId  is : </b> {user.githubId} <br/>
-            //           <b>Your name  is : </b> {user.name} <br/>
-            //           <b>Your email  is : </b> {user.email} <br/>
-            //           <b>Date  is : </b> {user.date} <br/>
-            //           <b>provider  is : </b> {user.provider} <br/>
-            //           <img src={user.thumbnail} width = "100" alt=""/>
-            //           <p className="flow-text grey-text text-darken-1">
-            //             You are logged into vg-tours{" "}
-            //             <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-            //           </p>
-            //         </h4>
-            //         </div>
-            // );
-         
+           );
           default:
             return "toto";
         }
@@ -217,7 +86,6 @@ export class Profile extends Component {
     componentDidMount() {
         this.props.socialLoginUser();
         console.log("this.props.match.params from componentDidMount",this.props)
-
 
         }
 
@@ -230,49 +98,15 @@ export class Profile extends Component {
           };
 
     render() {
-        const { isAuthenticated  } = this.props.auth;
+        //const { isAuthenticated  } = this.props.auth;
 
           return (
-            <div style={{ height: "75vh" }} className="container valign-wrapper">
-              
-              <div className="row">
-                <div className="col s12 center-align">
-                   {!isAuthenticated ? (
-        <Loader
-         type="Puff"
-         color="#00BFFF"
-         height={100}
-         width={100}
-         timeout={3000} //3 secs
-         />
-                ) : ( 
-                  <div>
-                    <h4>
-                  {this.getProfileNetworkUser()}
-                  <p className="flow-text grey-text text-darken-1">
-                      You are logged into vg-tours{" "}
-                      <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-                    </p>
-
-                    </h4>
-                    </div>
-                 )} 
-                  <button
-                    style={{
-                      width: "150px",
-                      borderRadius: "3px",
-                      letterSpacing: "1.5px",
-                      marginTop: "1rem"
-                    }}
-                    onClick={this.onLogoutClick}
-                    className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                  >
-                    Logout
-                  </button>
-                </div>
-              </div>
-            </div>
-          
+          <UserInfo
+          isAuthenticated = {this.props.auth} 
+          getProfileNetworkUser = {this.getProfileNetworkUser()}
+          onLogoutClick = {this.onLogoutClick} 
+           
+          />
           );
     }
 }
