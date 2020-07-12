5c3ffe0dcd5e6d9b4f6bfc0ea12d4792a8553258
SlimeVidas <dahan.isaac@gmail.com>
2020-07-12 21:21:20 +0300

    clean

--- client/src/components/HeaderMenu.js
+++ client/src/components/HeaderMenu.js
@@ -5,25 +5,8 @@ import FeaturesSubMenu from './FeaturesSubMenu'
 //   import '../assets/less/header.css'
 
 
-class  HeaderMenu extends Component {
-constructor(props) {
-   
+const HeaderMenu = (props) =>{
 
-    super(props)
-    console.log(this.props)
-    // console.log('header menu')
-
-    this.state = {
-       
-    }
-    // this.handleClick = this.handleClick.bind(this);
-}
-
-
-
-       
-
-    render(){
         return (
             <div className="item menu-default burger-mobile-sidemenu burger-tablet-sidemenu sidemenu-open-right icons-left profile-priority slide-out-sq dimmed flexMenu dropdown-open-right" data-burger="menu01">
     
@@ -35,14 +18,13 @@ constructor(props) {
             </Link>
         </li>
         
-        <li className="has-submenu">
+        {/* <li className="has-submenu">
             <Link to="#" className="item" >
                 <span>Features</span>
             </Link>
 
-          {/* Features sub menu */}
           <FeaturesSubMenu/>
-        </li>
+        </li> */}
         
         {/* <li>
         <Link to="/signup" className="item">
@@ -57,17 +39,13 @@ constructor(props) {
             </Link>
             </li>  */}
 
-            <li>
-            {/* <Link to={{ pathname: "/", search: "signupall=true" }} className="item"> */}
- 
+            {/* <li>
                  <Link to="/signup" className="item">
-                {/* <Link to={{ pathname: "/", search: "all=true" }} className="item"> */}
                 <span>Sign Up</span>
             </Link>
-           </li>
+           </li> */}
 
            <li>
-            {/* <Link to={{ pathname: "/", search: "signupall=true" }} className="item"> */}
  
                  <Link to="/connect" className="item">
                 {/* <Link to={{ pathname: "/", search: "all=true" }} className="item"> */}
@@ -81,28 +59,23 @@ constructor(props) {
             </Link>
             </li>  */}
 
-            <li>
+            {/* <li>
             <Link to={{ pathname: "/", search: "login=true" }} className="item">
             <span>Log In</span>
             </Link>
-            </li> 
+            </li>  */}
 
             <li>
             <Link to={{ pathname: "/", search: "signin=true" }} className="item">
             <span>Sign In</span>
             </Link>
             </li> 
-
-            
-
-    
     </ul>
     </div>
     
         )
     }
     
-}
 
 export default HeaderMenu
 
